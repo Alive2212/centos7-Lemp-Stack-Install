5052e71081abe4f4ec3954e4255ad23a55a335c9
Alive2212 <Alive2212@yahoo.com>
2017-07-17 10:37:53 +0430

    first commit

---
+++ installphp.sh
@@ -0,0 +1,4 @@
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+yum install -y mod_php71w php71w-opcache php71w-fpm php71w-common php71w-fpm php71w-mcrypt php71w-mysql php71w-pdo php71w-pecl-redis php71w-soap php71w-xml
